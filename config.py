1fa7135d26423b696780f9a46c18f3cbdc55e5d7
wls9611 <wls9611@gmail.com>
2026-02-19 22:55:44 +0900

    Update TICKERS list and retain trading thresholds

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 # --- 관심 종목 리스트 (TICKERS 변수가 반드시 있어야 함) ---
-TICKERS = [
-    "PLTR", "GOOGL", "CEG", "META", "MELI", "BRK-B", "CCJ", "APP",
-    "NLR", "QTUM", "XLV", "XLF", "XLU", "XLE", "XLP", "WQTM", "HTFL"
-]
+#TICKERS = [
+#   "PLTR", "GOOGL", "CEG", "META", "MELI", "BRK-B", "CCJ", "APP",
+#   "NLR", "QTUM", "XLV", "XLF", "XLU", "XLE", "XLP", "WQTM", "HTFL"
+#]
 
 #TICKERS = [
 #    "PLTR", "NVDA", "GOOGL", "META", 
 #    "AMZN", "TSLA", "AAPL", "MSFT"
 #]
+
+TICKERS = [
+    "QQQM", "SCHD", "SPYM"
+]
+
 
 # --- 매매 타점 기준값 ---
 RSI_OVERSOLD = 35    # 강력 매수 기준
@@ -17,6 +22,7 @@ RSI_OVERBOUGHT = 70  # 강력 매도 기준
 MFI_STRONG = 40      # 강력 매수 MFI
 
 MFI_WATCH = 55       # 매수 관찰 MFI
+
 
 
 
